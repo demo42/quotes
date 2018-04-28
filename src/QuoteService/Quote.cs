1138a7e1b22cb653577a614ff7f8075f8094e77e
Steve Lasker <StevenLasker@Hotmail.com>
2018-04-28 13:32:49 -0700

    Add version info for demonstrating changes

--- src/QuoteService/Quote.cs
+++ src/QuoteService/Quote.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations.Schema;
 namespace QuoteService
 {
     public class Quote
     {
+        public Quote(){
+            // var envVersion = new Version(Environment.GetEnvironmentVariable("VERSION"));
+            // if (envVersion != null){
+            //     Version=new Version(envVersion.ToString());
+            // } else{
+            //     Version=new Version("0.0.0");
+            // }
+        }
         public int Id { get; set; }
         public string Text { get; set; }
         public string Attribution { get; set; }
+        //[NotMapped]
+        //public Version Version { get; private set;}
     }
 }
